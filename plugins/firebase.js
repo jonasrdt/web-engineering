462b7f15353e3f8ef35843d782bcb49831d76fa4
Jonas Reinhardt <jonas.reinhardt@tricode.io>
2023-06-01 15:08:58 +0200

    updated firebasejs

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDI0ueTZKqXO1FE0rUeAwcNlUu51wgwgA0",
-    authDomain: "fawifhkiel.firebaseapp.com",
-    projectId: "fawifhkiel",
-    storageBucket: "fawifhkiel.appspot.com",
-    messagingSenderId: "424898989050",
-    appId: "1:424898989050:web:c78790b66b388f3e88b243"
+    apiKey: "enteryourdatafromfirebasehere",
+    authDomain: "enteryourdatafromfirebasehere",
+    projectId: "enteryourdatafromfirebasehere",
+    storageBucket: "enteryourdatafromfirebasehere",
+    messagingSenderId: "enteryourdatafromfirebasehere",
+    appId: "enteryourdatafromfirebasehere"
 };
 
 firebase.initializeApp(firebaseConfig)
